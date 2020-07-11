79508b9d35178de3252e55b59c79f20383369750
acseke <csekealpar12@gmail.com>
2020-07-11 12:33:17 +0300

    Updates

---
+++ precache-manifest.bd4eb7bc7aa14f9623843d3cad25d682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac83911bc648436c2ecb365ae01f2fe7",
+    "url": "/aggregate-charts/index.html"
+  },
+  {
+    "revision": "8fe8d4cd09801f3917b9",
+    "url": "/aggregate-charts/static/css/main.8677232e.chunk.css"
+  },
+  {
+    "revision": "549bb803b5e9b1107316",
+    "url": "/aggregate-charts/static/js/2.259ec58a.chunk.js"
+  },
+  {
+    "revision": "1c7d3e21e02d583b2570d8e1d082d913",
+    "url": "/aggregate-charts/static/js/2.259ec58a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe8d4cd09801f3917b9",
+    "url": "/aggregate-charts/static/js/main.6b220629.chunk.js"
+  },
+  {
+    "revision": "46e567ac46bddd685ff4",
+    "url": "/aggregate-charts/static/js/runtime-main.50777896.js"
+  }
+]);
